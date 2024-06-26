3bbf4811fa90100c17a280b9d52e832e5b234609
zjregee <zjregee@gmail.com>
2024-06-26 19:50:33 +0800

    feat: fix unattended install issues

--- scripts/install_and_run_test_iso.sh
+++ scripts/install_and_run_test_iso.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
-qemu-img create -f qcow2 ubuntu.qcow2 20G
-qemu-system-x86_64 -enable-kvm -smp 8 -m 16G -cdrom /tmp/ubuntu-20.04-unattended.iso -drive file=ubuntu.qcow2,format=qcow2 -boot d -nographic -no-reboot
+sudo apt install cloud-image-utils
+cloud-localds seed.iso user-data meta-data
+
+wget https://releases.ubuntu.com/20.04/ubuntu-20.04.6-live-server-amd64.iso
+
+sudo mkdir /mnt/ubuntu-iso
+sudo mount -o loop ubuntu-20.04.6-live-server-amd64.iso /mnt/ubuntu-iso
+
+truncate -s 20G image.img
+
+qemu-system-x86_64 -enable-kvm -smp 8 -m 16G \
+    -drive file=image.img,format=raw,cache=none,if=virtio \
+    -drive file=seed.iso,format=raw,cache=none,if=virtio \
+    -cdrom ubuntu-20.04.6-live-server-amd64.iso \
+    -kernel /mnt/ubuntu-iso/casper/vmlinuz \
+    -initrd /mnt/ubuntu-iso/casper/initrd \
+    -append "console=ttyS0 autoinstall" \
+    -nographic -no-reboot -boot d
+
+sudo umount /mnt/ubuntu-iso
+sudo rm -rf /mnt/ubuntu-iso
+
+qemu-system-x86_64 -enable-kvm -smp 8 -m 16G \
+    -drive file=image.img,format=raw,cache=none,if=virtio \
+    -nographic -boot c
