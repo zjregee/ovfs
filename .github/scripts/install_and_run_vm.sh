c66add789bc8c27d64d83b419d5c4042c721510a
zjregee <zjregee@gmail.com>
2024-08-18 19:20:32 +0800

    fix: update ci

--- .github/scripts/install_and_run_vm.sh
+++ .github/scripts/install_and_run_vm.sh
@@ -12,7 +12,7 @@ sudo mount -o loop ubuntu-20.04.6-live-server-amd64.iso /mnt/ubuntu-iso
 
 truncate -s 10G image.img
 
-qemu-system-x86_64 -enable-kvm -smp 2 -m 4G \
+sudo qemu-system-x86_64 -enable-kvm -smp 2 -m 4G \
     -drive file=image.img,format=raw,cache=none,if=virtio \
     -drive file=seed.iso,format=raw,cache=none,if=virtio \
     -cdrom ubuntu-20.04.6-live-server-amd64.iso \
@@ -24,7 +24,7 @@ qemu-system-x86_64 -enable-kvm -smp 2 -m 4G \
 sudo umount /mnt/ubuntu-iso
 sudo rm -rf /mnt/ubuntu-iso
 
-qemu-system-x86_64 -M pc -cpu host --enable-kvm -smp 2 \
+sudo qemu-system-x86_64 -M pc -cpu host --enable-kvm -smp 2 \
      -m 4G -object memory-backend-file,id=mem,size=4G,mem-path=/dev/shm,share=on -numa node,memdev=mem \
      -chardev socket,id=char0,path=/tmp/vfsd.sock -device vhost-user-fs-pci,queue-size=1024,chardev=char0,tag=myfs \
      -chardev stdio,mux=on,id=mon -mon chardev=mon,mode=readline -device virtio-serial-pci -device virtconsole,chardev=mon -vga none -display none \
